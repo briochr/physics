9f85e3ca10ba418b40e6f013968b462f79affd34
unvirusSDF <165483650+unvirusSDF@users.noreply.github.com>
2024-03-30 14:26:27 +0100

    20240330 edit files
    paint() cassé

---
+++ Graphic.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Graphic extends Frame {
+
+    public static void main(String[] args) {
+        System.out.println("helo there");
+        new Graphic();
+    }
+
+    public Graphic() {
+        windowLayout();
+    }
+
+    // Cette fonction est censée être appelée toute seule
+    // Ca a pas l'air de marcher :[
+    // A L'AIDE ANTOINE
+    public void paint(Graphics g) {
+        System.out.println("wow");
+        g.setColor(Color.WHITE);
+
+        Vector pos = new Vector(100, 100);
+        Vector move = new Vector(700, 400);
+        showVector(g, pos, move);
+    }
+
+    // Affiche le vecteur sur la fenêtre selon les coordonnées de départ et
+    // d'arrivée
+    public void showVector(Graphics g, Vector start, Vector end) {
+        int startX = (int) start.getX();
+        int startY = (int) start.getY();
+        int endX = (int) end.getX();
+        int endY = (int) end.getY();
+
+        g.drawLine(startX, startY, endX, endY);
+
+        int arrowSize = 10;
+        double angle = Math.atan2(endX - startX, endY - startY);
+        int[] xPoints = new int[3];
+        int[] yPoints = new int[3];
+        xPoints[0] = endX;
+        yPoints[0] = endY;
+        xPoints[1] = (int) (endX - arrowSize * Math.cos(angle - Math.PI / 6));
+        yPoints[1] = (int) (endY - arrowSize * Math.sin(angle - Math.PI / 6));
+        xPoints[2] = (int) (endX - arrowSize * Math.cos(angle + Math.PI / 6));
+        yPoints[2] = (int) (endY - arrowSize * Math.sin(angle + Math.PI / 6));
+        g.fillPolygon(xPoints, yPoints, 3);
+    }
+
+    // S'occupe de l'affichage des fenêtres
+    public void windowLayout() {
+        Frame f = new Frame();
+
+        Label l = new Label("salutations je suis un label rouge made in france", Label.CENTER);
+        l.setForeground(new Color(255, 255, 255));
+        l.setBounds(700, 100, 300, 100);
+
+        TextField tf = new TextField();
+        tf.setBounds(700, 200, 100, 40);
+
+        Button b = new Button("SUS");
+        b.setBounds(900, 200, 100, 40);
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("AMOGUS");
+            }
+        });
+
+        f.add(l);
+        f.add(b);
+        f.add(tf);
+
+        f.setTitle("Physics World");
+        f.setSize(1600, 900);
+        f.setBackground(new Color(0, 0, 0));
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
---
+++ PhysicsWorld.java
@@ -0,0 +1,137 @@
+// import java.util.Arrays;
+
+class Vector {
+    // Les vecteurs sont en 2D, ils n'ont comme composantes que x et y
+    private float x;
+    private float y;
+    private float norme;
+
+    // Le constructeur du vecteur
+    public Vector(float xCoor, float yCoor) {
+        this.x = xCoor;
+        this.y = yCoor;
+        this.norme = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    // Pour obtenir le x d'un vecteur
+    public float getX() {
+        return this.x;
+    }
+
+    // Pour obtenir le y d'un vecteur
+    public float getY() {
+        return this.y;
+    }
+
+    // Pour obtenir la norme d'un vecteur
+    public float getNorme() {
+        return this.norme;
+    }
+
+    // Pour modifier la valeur du x d'un vecteur
+    public void setX(float newX) {
+        this.x = newX;
+        this.norme = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    // Pour modifier la valeur du y d'un vecteur
+    public void setY(float newY) {
+        this.y = newY;
+        this.norme = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public void add(Vector v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.norme = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public void substract(Vector v) {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.norme = (float) Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public void multiply(float k) {
+        this.x *= k;
+        this.y *= k;
+        this.norme *= k;
+    }
+
+    public void divide(float k) {
+        this.x /= k;
+        this.y /= k;
+        this.norme /= k;
+    }
+
+    // opération vectorielle
+    public float det(Vector u) {
+        return (this.x * u.y) - (this.y * u.x);
+    }
+
+    public float scalar(Vector v) {
+        Vector u = new Vector(this.x, this.y);
+        u.substract(v);
+        return (float) (this.norme * this.norme + v.norme * v.norme - u.norme * u.norme);
+    }
+
+    // champs vectoriels
+    public Vector gravityField(float x, float y) {
+        return (new Vector(9.8f / ((x + 100) * (x + 100)), (9.8f / ((x + 100) * (x + 100)))));
+    }
+}
+
+class Polygon {
+    Vector[] lPts;
+
+    public Polygon(Vector[] listPoints) {
+        this.lPts = listPoints;
+    }
+
+    public float surface() {
+        float surface = 0;
+        if (this.lPts.length < 3) {
+            return surface;
+        } else
+            for (int i = 1; i < (this.lPts.length + 1); i++) {
+                Vector u = new Vector(this.lPts[i].getX() - this.lPts[0].getX(),
+                        this.lPts[i].getY() - this.lPts[0].getY());
+                surface += u.det(new Vector(this.lPts[i + 1].getX() - this.lPts[0].getX(),
+                        this.lPts[i].getY() - this.lPts[0].getY()));
+            }
+        return Math.abs(surface / 2f);
+    }
+}
+
+class Object {
+    Vector position;
+    Vector velocity;
+    Vector force; // force/mass = acceleration = velocity/time
+    Vector acceleration;
+    float mass;
+
+    // Les arguments (dans l'ordre) sont: position, velocité, force, masse.
+    public Object(Vector p, Vector v, Vector f, float m) {
+        this.position = p;
+        this.velocity = v;
+        this.force = f;
+        this.mass = m;
+    }
+}
+
+public class PhysicsWorld {
+
+    // La fonction main est appelée quand le programme se lance
+    public static void main(String[] args) {
+
+        Object testObj = new Object(new Vector(5, 3), new Vector(3, 0), new Vector(0, 1), 1);
+
+        System.out.println("position.x = " + testObj.position.getX());
+        System.out.println("force.y = " + testObj.force.getY());
+
+        testObj.velocity.setX(69);
+        System.out.println(testObj.velocity.getX());
+        testObj.position.setY(420);
+        System.out.println(testObj.position.getY());
+    }
+}
