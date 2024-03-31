c3280a3f348a91a933e1bc3499217e35bfaba0e5
briochr <75985190+briochr@users.noreply.github.com>
2024-03-31 15:07:47 +0200

    Create Polygon.java

---
+++ Polygon.java
@@ -0,0 +1,11 @@
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class Polygon{
+  ArrayList<String> fruits = new ArrayList<>();
+  fruits.add("apple");
+  fruits.add("une BANANE !");
+
+  for (String fruit : fruits){
+    System.out.println(fruit);
+}
